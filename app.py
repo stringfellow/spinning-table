7ad6d9c2316b2ea7dc43ba5d15b88b062d7e61d2
Steve Pike <steve.pike@carbonculture.net>
2014-09-18 19:54:00 +0100

    partial commit - this code is trash and v unsafe, but might work as a base

--- app.py
+++ app.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 import os
 
-import momoko
+from datetime import datetime, timedelta
+
+import psycopg2
+#import momoko
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
@@ -16,31 +19,36 @@ define("host", default='0.0.0.0', help="run on the given inet", type=str)
 db_database = os.environ.get('SPINNING_DB', '')
 db_user = os.environ.get('SPINNING_USER', 'postgres')
 db_password = os.environ.get('SPINNING_PASSWORD', '')
-db_host = os.environ.get('SPINNING_HOST', '')
+db_host = os.environ.get('SPINNING_HOST', '127.0.0.1')
 db_port = os.environ.get('SPINNING_PORT', 5432)
 enable_hstore = True if os.environ.get(
     'SPINNING_HSTORE', False) == '1' else False
-dsn = 'dbname=%s user=%s password=%s host=%s port=%s' % (
-    db_database, db_user, db_password, db_host, db_port)
+dsn = 'dbname=%s user=%s host=%s port=%s' % (
+    db_database, db_user, db_host, db_port)
+if db_password:
+    dsn += ' password=' + db_password
+print dsn
 
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/", Main),
-            (r"/websocket/", QueryTable),
+            (r"/([^/]+)/", Main),
+            (r"/websocket", QueryTable),
         ]
         settings = dict(
-            static_path=os.path.join(os.path.abspath('.'), 'templates')
+            static_path=os.path.join(os.path.abspath('.'), 'static'),
+            template_path=os.path.join(os.path.dirname('.'), "templates"),
         )
         tornado.web.Application.__init__(self, handlers, **settings)
-        self.db = momoko.Pool(
-            dsn=dsn,
-            size=1,
-            max_size=3,
-            setsession=("SET TIME ZONE UTC",),
-            raise_connect_errors=False,
-        )
+#        self.db = momoko.Pool(
+#            dsn=dsn,
+#            size=1,
+#            max_size=3,
+#            setsession=("SET TIME ZONE UTC",),
+#            raise_connect_errors=False,
+#        )
+        self.db = psycopg2.connect(dsn)
 
 
 class DbMixin(object):
@@ -48,23 +56,33 @@ class DbMixin(object):
     def db(self):
         return self.application.db
 
+    @property
+    def cursor(self):
+        return self.db.cursor()
+
 
 class QueryTable(tornado.websocket.WebSocketHandler, DbMixin):
     def open(self):
-        pass
-        # clients.append(self)
+        self.last_time = datetime.utcnow() - timedelta(seconds=60 * 60)
 
     def on_message(self, message):
-        pass
-
-    def on_close(self):
-        pass
-        # clients.remove(self)
+        new_now = datetime.utcnow()
+        elapsed = new_now - self.last_time
+        # TODO: check this table exists in the db
+        self.cursor.execute(
+            "SELECT * FROM {0} WHERE last_login >= %s".format(message),
+            (self.last_time,),
+        )
+        try:
+            self.write_message('Query results: %s<br>' % self.cursor.fetchall())
+            self.last_time = new_now
+        except:
+            self.write_message('[]')
 
 
 class Main(tornado.web.RequestHandler, DbMixin):
-    def get(self):
-        self.render("querytable.html")
+    def get(self, table):
+        self.render("querytable.html", table=table)
 
 
 def main():
